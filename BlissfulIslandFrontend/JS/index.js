35bae8b30fd1abacd6fb9313ff0c67da6dc76895
robertjgoette <54007387+robertjgoette@users.noreply.github.com>
2021-06-25 14:40:25 -0500

    Basic NavBar functions and buttons
    Basic NavBar functions and buttons. Imgs still need to be added.

---
+++ BlissfulIslandFrontend/JS/index.js
@@ -0,0 +1,21 @@
+// Start of NavBar JS code
+if(accountType === 1){
+    fillNavBar()
+}
+function fillNavBar(){
+    let accountMakerButton = "<a id='accountMakerButton' onclick=accountMakerPage()>Account Maker</a>";
+    let unitListButton = "<a id='unitListButton' onclick=unitListPage()>Unit List</a>";
+
+    accountMaker.innerHTML = accountMakerButton;
+    unitList.innerHTML = unitListButton;
+}
+window.onscroll = function() {
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+        navBar.className = "navBar--scroll";
+        navBarGroup.className = "navBar__group--scroll";
+    } else {
+        navBar.className = "navBar"; 
+        navBarGroup.className  = "navBar__group";
+    }	
+}
+// End of NavBar JS code
